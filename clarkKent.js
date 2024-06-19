f8a80d3c7fc95da429f0a58750b12ca72fd578de
anyoneout <chrisdafur@gmail.com>
2024-06-19 15:38:51 -0500

    added clarkKent html and js

---
+++ clarkKent.js
@@ -0,0 +1,6 @@
+function clarkKentChanger() {
+    myClarkKentHeader.innerHTML = "Superman";
+    myClarkKentParagraph.innerHTML = "It's amazing what a pair of glasses can conceal."
+    }
+
+mySupermanButton.onclick = clarkKentChanger;
