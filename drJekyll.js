3138ed924512e9cd3adaf512115428b60ac14812
anyoneout <chrisdafur@gmail.com>
2024-06-19 16:06:23 -0500

    added drjekyll  html and js

---
+++ drJekyll.js
@@ -0,0 +1,10 @@
+function drJekyllChanger() {
+    myDrJekyllHeader.innerHTML = "Mr. Hyde";
+    myDrJekyllHeader.style.color = "white";
+    myDrJekyllHeader.style.fontFamily = "garamond";
+    myDrJekyllHeader.style.backgroundColor = "purple";
+    myDrJekyllParagraph.innerHTML = "We're gonna need a new pair of pants.";
+    myDrJekyllButton.innerHTML = "Now where is Griffin?"
+    }
+
+myDrJekyllButton.onclick = drJekyllChanger;
